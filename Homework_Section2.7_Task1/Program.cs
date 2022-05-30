561df3990b87d521ca3e257947308114ca3e2117
aorlov-96 <rtolyaorlov@yandex.ru>
2022-05-30 13:11:37 +0300

    Сделал 2 задание

--- Homework_Section2.7_Task1/Program.cs
+++ Homework_Section2.7_Task1/Program.cs
@@ -22,9 +22,21 @@ namespace task1
                               email,
                               Program_Scores,
                               Math_Scores,
-                              Physics_Scores);
+                             Physics_Scores);
+
+            Console.WriteLine("Нажмите любую клавишу");
             Console.ReadKey();
 
+            double Sum = (Program_Scores + Math_Scores + Physics_Scores); // Обявляю переменную и сразу считаю сумму
+            double Average = (Sum / 3); // Объявляю переменную и считаю среднее арифмитическое, хоте заменить 3 на подсчет количества 
+                                        // переменных, но не знаю как это сделать без цикла for.
+            string AverageString = Average.ToString("#.###"); //Перевожу в string, чтобы выводить на экран 3 точки после запятой.
+
+            Console.WriteLine($"Сумма всех предметов = {Sum}");
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadKey();
+            Console.WriteLine($"Среднее арифметическое по 3 предметам = {AverageString}");
+            Console.ReadKey();
 
 
 
