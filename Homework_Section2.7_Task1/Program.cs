717967f4d738196bf71944c6bc8df64b781e6b8f
zckm1 <zckm1@LAPTOP-TOEGG1EE>
2022-05-30 12:29:01 +0300

    Добавьте файлы проекта.

---
+++ Homework_Section2.7_Task1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string FullName = "Петров Иван Иванович";
+            int Age = 18;
+            string email = "Petrov1996@gmail.com";
+            double Program_Scores = 78.5;
+            double Math_Scores = 98;
+            double Physics_Scores = 85;
+
+            string pattern = "Ф.И.О.: {0} \nВозраст: {1} \nemail: {2} \nБаллы по программированию {3} \nБаллы по математике: {4} \nБаллы по физике: {5}";
+
+            Console.WriteLine(pattern,
+                              FullName,
+                              Age,
+                              email,
+                              Program_Scores,
+                              Math_Scores,
+                              Physics_Scores);
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
